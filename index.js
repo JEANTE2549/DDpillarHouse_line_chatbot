8471c74f0dc92b212068556d129f6d3da7ed52d0
Kanatip Jaksarn <123433032+JEANTE2549@users.noreply.github.com>
2025-08-03 16:37:05 +0700

    Setup Line Chatbot env

---
+++ index.js
@@ -0,0 +1,23 @@
+require('dotenv').config();
+const express = require('express');
+const line = require('@line/bot-sdk');
+const webhookHandler = require('./webhook');
+const app = express();
+const cors = require('cors');
+const { createClient } = require('@supabase/supabase-js');
+
+const config = {
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.CHANNEL_SECRET
+};
+
+app.use('/api', cors({
+  origin: '*'
+}));
+app.use('/api', express.json());
+app.post('/webhook', line.middleware(config), webhookHandler(config));
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_KEY
+);
